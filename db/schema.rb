cfa023469cfa530ace52f27ce25b58b7f79ed92c
Sadique <sadiquea346@gmail.com>
2020-01-03 13:04:05 +0500

    Association Created Between user and product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_072931) do
+ActiveRecord::Schema.define(version: 2020_01_03_080230) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_01_03_072931) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
